44e3288bc1c4fbcdc269db4374afbe9e0b6d31af
jihee <bonjenny1@gmail.com>
2021-07-14 16:01:07 +0900

    repository

--- src/main/java/logini/coco/config/WebSecurityConfig.java
+++ src/main/java/logini/coco/config/WebSecurityConfig.java
@@ -2,47 +2,51 @@ package logini.coco.config;
 
 import logini.coco.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @RequiredArgsConstructor
-@EnableWebSecurity  //springSecurity 사용
+@EnableWebSecurity
 @Configuration
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
 
     @Override
-    public void configure(WebSecurity web){     //보안무시하는 페이지
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**","/css/main.css");
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/login", "signup", "/main").permitAll()   //누구나
-                .antMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated() //나머지 요청은 권한이 있어야함
-            .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/main") // 성공하면 다시 메인으로
-            .and()
-                .logout()
-                    .logoutSuccessUrl("/login")
-                    .invalidateHttpSession(true)
-                ;
+    protected void configure(HttpSecurity http) throws Exception{
+            http
+                    .authorizeRequests()
+                        .antMatchers("/login", "/signup", "/user").permitAll()
+                        .antMatchers("/").hasRole("USER")
+                        .antMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                    .and()
+                        .formLogin()
+                            .loginPage("/login")
+                            .defaultSuccessUrl("/")
+                    .and()
+                        .logout()
+                            .logoutSuccessUrl("/login")
+                            .invalidateHttpSession(true);
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userService)
-                .passwordEncoder(new BCryptPasswordEncoder());
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
+
 }
--- src/main/java/logini/coco/entity/UserInfo.java
+++ src/main/java/logini/coco/entity/UserInfo.java
@@ -4,9 +4,14 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 // noArgs =파라미터 없는 기본 생성자 생성 + 접근 제어 protected
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -31,12 +36,50 @@ public class UserInfo implements UserDetails {
     @Column(name = "hobby")
     public String hobby;
 
+    @Column(name = "auth")
+    private String auth;
+
     @Builder
-    public UserInfo(String email,String name, String password, String addr, String hobby) {
+    public UserInfo(String email,String name, String password, String addr, String hobby, String auth) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.addr =addr;
         this.hobby = hobby;
+        this.auth = auth;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        Set<GrantedAuthority> roles = new HashSet<>();
+        for (String role : auth.split(",")){
+            roles.add(new SimpleGrantedAuthority(role));
+        }
+        return roles;
+    }
+
+    @Override
+    public String getUsername(){
+        return email;
+    }
+    @Override
+    public String getPassword(){
+        return password;
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isEnabled(){
+        return true;
     }
 }
