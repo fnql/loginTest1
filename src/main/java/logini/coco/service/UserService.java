39aad7b5340c6bf0599c4e2612788a812dbfc834
woogi <dbwhddnr00@naver.com>
2021-07-17 17:11:52 +0900

    UserService~

--- src/main/java/logini/coco/service/UserService.java
+++ src/main/java/logini/coco/service/UserService.java
@@ -1,4 +1,21 @@
 package logini.coco.service;
 
-public class UserService {
+import logini.coco.entity.UserInfo;
+import logini.coco.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserInfo loadUserByUsername(String email) throws UsernameNotFoundException{
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
 }
