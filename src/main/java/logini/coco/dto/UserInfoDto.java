cd717cfb871382d6477c34c7dc8ac4c0bfefe5d5
jihee <bonjenny1@gmail.com>
2021-07-23 16:03:32 +0900

    return 받기

--- src/main/java/logini/coco/dto/UserInfoDto.java
+++ src/main/java/logini/coco/dto/UserInfoDto.java
@@ -3,14 +3,25 @@ package logini.coco.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class UserInfoDto {
 
+    @NotBlank(message = "이메일 주소를 입력해주세요.")
+    @Email(message = "올바른 이메일 주소를 입력해주세요.")
     private String email;
+
     private String password;
     private String auth;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
     private String name;
+
     private String hobby;
     private String addr;
 
