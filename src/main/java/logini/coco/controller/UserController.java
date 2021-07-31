e1ec176a9993290e887d5e4ebffa0d5494464ef7
woogi <dbwhddnr00@naver.com>
2021-07-31 22:23:22 +0900

    log4 확인중

--- src/main/java/logini/coco/controller/UserController.java
+++ src/main/java/logini/coco/controller/UserController.java
@@ -47,14 +47,12 @@ public class UserController {
     }
 
     //이메일 인증
-    @RequestMapping(value = "/mailCheck", method = RequestMethod.GET)
+    @RequestMapping(value="/mailCheck", method=RequestMethod.GET)
     @ResponseBody
-    public void mailCheckGET(@RequestParam("eMail") String email) {
+    public void mailCheckGET(String email) throws Exception{
         /* 뷰(View)로부터 넘어온 데이터 확인 */
         logger.info("이메일 데이터 전송 확인");
         logger.info("인증번호 : " + email);
 
-        Random random = new Random();
-        int checkNum = random.nextInt(888888) + 111111;
     }
 }
