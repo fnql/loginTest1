680f41036fb13bddf39073608ea12d025c1280d0
fnql <bonjenny1@gmail.com>
2021-08-11 15:50:06 +0900

    ajax type change

--- src/main/java/logini/coco/controller/ApiController.java
+++ src/main/java/logini/coco/controller/ApiController.java
@@ -2,10 +2,7 @@ package logini.coco.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 
@@ -15,7 +12,7 @@ public class ApiController {
     @Value("${kakao.js}")
     String kakaoApi;
 
-    @RequestMapping(value = "/getKakaoApi", method = RequestMethod.POST )
+    @GetMapping("/getKakaoApi")
     public String getKakaoApi()  {
         System.out.println("getKakaoApi");
         return kakaoApi;
