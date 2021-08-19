6620ffa789333087bdc75006f97759a31b4ac66d
woogi <dbwhddnr00@naver.com>
2021-08-19 21:14:43 +0900

    구글 로그인 중단

--- src/main/java/logini/coco/controller/UserController.java
+++ src/main/java/logini/coco/controller/UserController.java
@@ -1,5 +1,6 @@
 package logini.coco.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import logini.coco.dto.MailDto;
 import logini.coco.dto.UserInfoDto;
 import logini.coco.service.MailService;
@@ -12,15 +13,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import static logini.coco.service.MailService.*;
@@ -79,6 +85,39 @@ public class UserController {
 
         String num = Integer.toString(checkNum);
         return num;
+    }
+
+    @RequestMapping(value="/userNaverLoginPro.do",  method = {RequestMethod.GET,RequestMethod.POST})
+    public String userNaverLoginPro(Model model, @RequestParam Map<String,Object> paramMap, @RequestParam String code, @RequestParam String state, HttpSession session) throws SQLException, Exception {
+        System.out.println("paramMap:" + paramMap);
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        OAuth2AccessToken oauthToken;
+        oauthToken = naverloginbo.getAccessToken(session, code, state);
+        //로그인 사용자 정보를 읽어온다.
+        String apiResult = naverloginbo.getUserProfile(oauthToken);
+        System.out.println("apiResult =>"+apiResult);
+        ObjectMapper objectMapper =new ObjectMapper();
+        Map<String, Object> apiJson = (Map<String, Object>) objectMapper.readValue(apiResult, Map.class).get("response");
+
+        Map<String, Object> naverConnectionCheck = userService.naverConnectionCheck(apiJson);
+
+        if(naverConnectionCheck == null) { //일치하는 이메일 없으면 가입
+
+            model.addAttribute("email",apiJson.get("email"));
+            model.addAttribute("password",apiJson.get("id"));
+            model.addAttribute("phone",apiJson.get("mobile"));
+            return "user/setNickname";
+        }else if(naverConnectionCheck.get("NAVERLOGIN") == null && naverConnectionCheck.get("EMAIL") != null) { //이메일 가입 되어있고 네이버 연동 안되어 있을시
+            userService.setNaverConnection(apiJson);
+            Map<String, Object> loginCheck = userService.userNaverLoginPro(apiJson);
+            session.setAttribute("userInfo", loginCheck);
+        }else { //모두 연동 되어있을시
+            Map<String, Object> loginCheck = userService.userNaverLoginPro(apiJson);
+            session.setAttribute("userInfo", loginCheck);
+        }
+
+        return "redirect:usermain.do";
     }
 
 }
